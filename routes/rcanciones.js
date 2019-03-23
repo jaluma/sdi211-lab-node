d264276bd849c9828d9abdfdcad012c739a66f2a
Javi Martinez <javiermartinezalvarez98@gmail.com>
2019-03-23 16:38:30 +0100

    SDI-NodeJS-8.9 y 8.10 - Colecciones relacionadas y routers
    Signed-off-by: Javi Martinez <javiermartinezalvarez98@gmail.com>

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,37 +1,30 @@
 module.exports = function (app, swig, gestorBD) {
-    app.get("/canciones", function (req, res) {
-        const canciones = [{
-            "nombre": "Blank space",
-            "precio": "1.2"
-        }, {
-            "nombre": "See you again",
-            "precio": "1.3"
-        }, {
-            "nombre": "Uptown Funk",
-            "precio": "1.1"
-        }];
-        const respuesta = swig.renderFile('views/btienda.html', {
-            vendedor: 'Tienda de canciones',
-            canciones: canciones
-        });
-        res.send(respuesta);
-    });
 
     app.get('/canciones/agregar', function (req, res) {
-        if (req.session.usuario == null) {
-            res.redirect("/tienda");
-            return;
-        }
         res.send(swig.renderFile('views/bagregar.html', {}));
+    });
+
+    app.get('/cancion/modificar/:id', function (req, res) {
+        let criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
+        gestorBD.obtenerCanciones(criterio, function (canciones) {
+            if (canciones == null) {
+                res.send(respuesta);
+            } else {
+                let respuesta = swig.renderFile('views/bcancionModificar.html',
+                    {
+                        cancion: canciones[0]
+                    });
+                res.send(respuesta);
+            }
+        });
     });
 
     app.get('/cancion/:id', function (req, res) {
         const criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
         gestorBD.obtenerCanciones(criterio, function (canciones) {
-            let respuesta = swig.renderFile('views/bcancion.html',
-                {
-                    cancion: canciones[0]
-                });
+            let respuesta = swig.renderFile('views/bcancion.html', {
+                cancion: canciones[0]
+            });
             if (canciones == null) {
                 res.send(respuesta);
             } else {
@@ -41,11 +34,6 @@ module.exports = function (app, swig, gestorBD) {
     });
 
     app.post("/cancion", function (req, res) {
-        if (req.session.usuario == null) {
-            res.redirect("/tienda");
-            return;
-        }
-
         let cancion = {
             nombre: req.body.nombre,
             genero: req.body.genero,
@@ -81,8 +69,33 @@ module.exports = function (app, swig, gestorBD) {
         });
     });
 
+    app.post('/cancion/modificar/:id', function (req, res) {
+        let id = req.params.id;
+        let criterio = {"_id": gestorBD.mongo.ObjectID(id)};
+
+        let cancion = {
+            nombre: req.body.nombre,
+            genero: req.body.genero,
+            precio: req.body.precio
+        };
+
+        gestorBD.modificarCancion(criterio, cancion, function (result) {
+            if (result == null) {
+                res.send("Error al modificar ");
+            } else {
+                paso1ModificarPortada(req.files, id, function (result) {
+                    if (result == null) {
+                        res.send("Error en la modificación");
+                    } else {
+                        res.send("Modificado");
+                    }
+                })
+            }
+        });
+    });
+
     app.get("/tienda", function (req, res) {
-        var criterio = {};
+        let criterio = {};
         if (req.query.busqueda != null) {
             criterio = {"nombre": {$regex: ".*" + req.query.busqueda + ".*"}};
         }
@@ -90,11 +103,57 @@ module.exports = function (app, swig, gestorBD) {
             if (canciones == null) {
                 res.send("Error al listar ");
             } else {
-                var respuesta = swig.renderFile('views/btienda.html', {
+                let respuesta = swig.renderFile('views/btienda.html', {
                     canciones: canciones
                 });
                 res.send(respuesta);
             }
         });
     });
+
+    app.get("/publicaciones", function (req, res) {
+        let criterio = {autor: req.session.usuario};
+        gestorBD.obtenerCanciones(criterio, function (canciones) {
+            if (canciones == null) {
+                res.send("Error al listar ");
+            } else {
+                let respuesta = swig.renderFile('views/bpublicaciones.html', {
+                    canciones: canciones
+                });
+                res.send(respuesta);
+            }
+        });
+    });
+
+    /* AUXILIARES */
+
+    function paso1ModificarPortada(files, id, callback) {
+        if (files.portada != null) {
+            let imagen = files.portada;
+            imagen.mv('public/portadas/' + id + '.png', function (err) {
+                if (err) {
+                    callback(null); // ERROR
+                } else {
+                    paso2ModificarAudio(files, id, callback); // SIGUIENTE
+                }
+            });
+        } else {
+            paso2ModificarAudio(files, id, callback); // SIGUIENTE
+        }
+    }
+
+    function paso2ModificarAudio(files, id, callback) {
+        if (files.audio != null) {
+            let audio = files.audio;
+            audio.mv('public/audios/' + id + '.mp3', function (err) {
+                if (err) {
+                    callback(null); // ERROR
+                } else {
+                    callback(true); // FIN
+                }
+            });
+        } else {
+            callback(true); // FIN
+        }
+    }
 };
